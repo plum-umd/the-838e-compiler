a12f53f28e9321b248173e10182cc3bfe12140b5
David Van Horn <dvanhorn@cs.umd.edu>
2021-02-06 14:29:10 -0500

    Try again.

--- villain/try-ws.rkt
+++ villain/try-ws.rkt
@@ -1,5 +1,12 @@
 #lang racket
-(system "make try.o")
+;(system "make try.o")
+(require "compile-file.rkt")
+(require (submod a86/printer private))
+(parameterize ((current-shared? #t))
+  (with-output-to-file "try.o"
+    (lambda ()  (main "try.rkt"))
+    #:exists 'replace))
+
 (system "make runtime.o")
 (system "gcc -z defs -v -lunistring -shared try.o runtime.o -o try.so")
 
