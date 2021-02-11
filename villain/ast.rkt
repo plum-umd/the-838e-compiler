4b1c58de5852e9b7a40fdb8f7b794ea039a0849f
dandorat <42868961+dandorat@users.noreply.github.com>
2021-02-11 13:08:09 -0500

    Packed string representation with string functions implemented (#21)
    * Packed string representation with string functions implemented
    
    * fixes done, new implementation of
    string-ref, string-set!, and make-string. Please see comments in string-set! and regarding
    its test cases in test-runner.rkt
    
    * "Updated compile.rkt, replaced r11 register with r8 in make-string, added test cases."
    
    Co-authored-by: David Van Horn <dvanhorn@cs.umd.edu>

--- villain/ast.rkt
+++ villain/ast.rkt
@@ -12,31 +12,39 @@
 ;;           | (Int Integer)
 ;;           | (Bool Boolean)
 ;;           | (Char Character)
+;;           | (String)         
 ;;           | (Prim0 Op0)
 ;;           | (Prim1 Op1 Expr)
-;;           | (Prim1 Op2 Op2 Expr)
+;;           | (Prim2 Op2 Expr Expr)      
+;;           | (Prim3 Op3 Expr Expr Expr)     
 ;;           | (If Expr Expr Expr)
 ;;           | (Begin Expr Expr)
 ;;           | (Let Id Expr Expr)
 ;;           | (Var Id)
 ;;           | (App Id (Listof Expr))
+
 ;; type Id   = Symbol
 ;; type Op0  = 'read-byte | 'void | 'collect-garbage
 ;; type Op1  = 'add1 | 'sub1 | 'zero?
 ;;           | 'char? | 'integer->char | 'char->integer
 ;;           | 'write-byte | 'eof-object?
 ;;           | 'box | 'car | 'cdr | 'unbox
+;;           | 'string-length | 'string? | make-string     
 ;;           | 'empty?
 ;; type Op2  = '+ | '- | 'eq?
-;;           | 'cons
+;;           | 'cons | 'string-ref               
+;; type Op3  = 'string-set!                    
+
 (struct Eof   ()           #:prefab)
 (struct Empty ()           #:prefab)
 (struct Int   (i)          #:prefab)
 (struct Bool  (b)          #:prefab)
 (struct Char  (c)          #:prefab)
+(struct String (s)         #:prefab)   
 (struct Prim0 (p)          #:prefab)
 (struct Prim1 (p e)        #:prefab)
 (struct Prim2 (p e1 e2)    #:prefab)
+(struct Prim3 (p e1 e2 e3) #:prefab)
 (struct If    (e1 e2 e3)   #:prefab)
 (struct Begin (e1 e2)      #:prefab)
 (struct Let   (x e1 e2)    #:prefab)
--- villain/interp-prims.rkt
+++ villain/interp-prims.rkt
@@ -1,6 +1,6 @@
 #lang racket
 (require "ast.rkt")
-(provide interp-prim1 interp-prim2)
+(provide interp-prim1 interp-prim2 interp-prim3)
 
 ;; Op1 Value -> Answer
 (define (interp-prim1 p1 v)
@@ -18,6 +18,8 @@
     [(list 'unbox (? box?))               (unbox v)]
     [(list 'car (? pair?))                (car v)]
     [(list 'cdr (? pair?))                (cdr v)]
+    [(list 'string-length (? string?))    (string-length v)]   
+    [(list 'string? v)                    (string? v)]       
     [(list 'empty? v)                     (empty? v)]
     [(list 'char-whitespace? (? char?))   (char-whitespace? v)]
     [_                                    'err]))
@@ -29,6 +31,21 @@
     [(list '- (? integer?) (? integer?))  (- v1 v2)]
     [(list 'eq? v1 v2)                    (eqv? v1 v2)]
     [(list 'cons v1 v2)                   (cons v1 v2)]
+    [(list 'string-ref
+           (? string?) (? integer?))      (if (<= 0 v2 (sub1 (string-length v1)))
+                                              (string-ref v1 v2)
+                                              'err)]   
+    [(list 'make-string
+           (? integer?) (? char?))        (if (< v1 0) 'err (make-string v1 v2))]                         
+    [_                                    'err]))
+
+;; Op3 Value Value Value -> Answer
+(define (interp-prim3 p v1 v2 v3)
+  (match (list p v1 v2 v3)
+    [(list 'string-set! (? string?)
+           (? integer?) (? char?))        (if (<= 0 v2 (sub1 (string-length v1)))
+                                              (string-set! v1 v2 v3)
+                                              'err)]  
     [_                                    'err]))
 
 ;; Any -> Boolean
