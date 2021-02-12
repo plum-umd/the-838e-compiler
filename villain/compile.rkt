9d3206c54a5670e0fc713ffd0f9ea8d4e3bc95b8
Olasubomi Efuniyi <subsefun@gmail.com>
2021-02-12 09:06:32 -0500

    Modified compiling match to raise error if no match case if found. Added tests for this

--- villain/compile.rkt
+++ villain/compile.rkt
@@ -264,7 +264,9 @@
   (let ((continue (gensym "continue")))
     (match (cons ps es)
       [(cons '() '())
-       (seq)]
+       (seq
+        (%% "If the program reaches here, then all the patterns have been exhausted and this is a match error")
+        (Jmp 'raise_error))]
       [(cons (cons (? value? v) ps) (cons h es))
        (seq
         (compile-value (extract-literal v))
