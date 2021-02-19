8db0039fadfb0d203bff0bd245899be5a3e29182
Olasubomi Efuniyi <subsefun@gmail.com>
2021-02-19 10:51:01 -0500

    Trying to confirm that it is the compiler that is failing the float test for pattern matching

--- villain/test/compile.rkt
+++ villain/test/compile.rkt
@@ -11,8 +11,8 @@
 (current-objs
  (list (path->string (normalize-path "../runtime.o"))))
 
-(test-runner    (λ (e) (unload/free (asm-interp (compile (parse e))))))
+#|(test-runner    (λ (e) (unload/free (asm-interp (compile (parse e))))))
 (test-runner-io (λ (e s)
                   (match (asm-interp/io (compile (parse e)) s)
                     ['err 'err]
-                    [(cons r o) (cons (unload/free r) o)])))
+                    [(cons r o) (cons (unload/free r) o)])))|#
