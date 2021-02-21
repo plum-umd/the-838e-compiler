72a9f86a8ee327aa8c2ea7f7684204b5874972dc
Danna Doratotaj <ddoratot@terpmail.umd.edu>
2021-02-20 21:39:46 -0500

    Replaced fixed-size char arrays with malloc for arbitrarily long file names

--- villain/formdps.c
+++ villain/formdps.c
@@ -5,9 +5,9 @@ int main (int argc, char *argv[]) {
 	char * two = "2";
 	char * cspace = " ";
         char * move = "mv ";
-	char makerun2[200];
+	char * makerun2;
         char * tmp;
-        char mvrun2[200];
+        char * mvrun2;
         int len;
         if (strcmp(argv[1], "mv") == 0){
            len = strlen(argv[2]);
@@ -17,6 +17,9 @@ int main (int argc, char *argv[]) {
            len--;
            strncpy(tmp, argv[2], len);
            tmp[len] = '\0';
+           if (!(mvrun2 = malloc (2 * len + 6))){
+              printf("malloc error");
+           }
            strcpy(mvrun2, argv[1]);
            strcat(mvrun2, cspace);
            strcat(mvrun2, argv[2]);
@@ -26,6 +29,10 @@ int main (int argc, char *argv[]) {
            printf("%s\n", mvrun2);
            system(mvrun2);
         } else {
+           len = strlen(argv[2]);
+           if (!(makerun2 = malloc (len + 7))){
+              printf("malloc error");
+           }
 	   strcpy(makerun2, argv[1]);
 	   strcat(makerun2, cspace);
 	   strcat(makerun2, argv[2]);
